fcc3e211dd73e9e4889e63b23aa3b03f0e188661
Class04 <hassankhan5uy99@gmail.com>
2024-01-21 01:31:19 +0500

    sexth-commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,65 +5,51 @@ import playbtn from "./assets/playButton.png";
 import namepng from "./assets/name.png";
 import mainimg from "./assets/main2imge.jpg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="h-[100%] w-[100%]">
-      <nav className="flex justify-between shadow-md h-[70px] p-4 items-center ">
-        <div className="flex items-center justify-center ml-10">
-          <span className="w-[25px] h-[25px] rounded-2xl  bg-orange-500"></span>
-          <h2 className="font-bold text-2xl ml-3">Hassan</h2>
-          <span className="text-md ml-2 mt-1">Web dev</span>
-        </div>
-        <div className="me-12">
-          <a href="" className="mx-2">
-            Resume
-          </a>
-          |
-          <a href="" className="mx-2">
-            Projects
-          </a>
-          |
-          <a href="" className="mx-2">
-            Contact
-          </a>
-        </div>
-      </nav>
+      <Navbar />
       {/* Home */}
-      <div className="h-[100%] w-[100%]  flex mt-32 items-center flex-col lg:flex-row">
-        <div className="h-[100%] w-[50%]  flex items-center justify-end p-8">
-          <img src={mainimg} className="w-[60%]  rounded-full grayscale" />
+      <div className="h-[100%] w-[100%]  flex mt-32 items-center  flex-col-reverse lg:flex-row ">
+        <div className="h-[100%] w-[100%]  flex items-center justify-center p-8 lg:justify-end ">
+          <img
+            src={mainimg}
+            className="w-[100%] h-[100%]  rounded-full grayscale lg:w-[50%]"
+          />
         </div>
 
-        <div className="h-full w-[50%] px-8 ">
-          <div className="h-full w-full">
-            <div className="mb-12">
-              <h2 className="font-bold text-8xl">Hello</h2>
+        <div className="h-full w-full p-6 flex justify-center lg:justify-start ">
+          <div className="">
+            <div className=" mb-6 lg:mb-12">
+              <h2 className="text-7xl font-bold lg:text-8xl">
+                Hello
+              </h2>
             </div>
             <div className="mt-2">
-              <h3 className="text-2xl font-bold">A Bit About Me</h3>
-              <p className="text-lg ">
-                Lorem ipsum dolor sit amet, consectetur <br />
-                Lorem ipsum dolor sit amet, consectetur dolor
-                <br />
-                Lorem ipsum dolor amet, consectetur
+              <h3 className="text-2xl font-bold mb-2">A Bit About Me</h3>
+              <p className="text-lg tracking-wider text-justify">
+                Enthusiastic React.js developer with a knack for <br /> building
+                engaging user interfaces, proficient in <br /> creating seamless and
+                responsive web applications.
               </p>
             </div>
-            <div className="flex w-full h-full mt-10">
-              <div className="w-[180px] h-[180px]  bg-orange-400  rounded-full  ml-3 flex items-center justify-center">
+            <div className="flex justify-center  h-full mt-10 w-full lg:justify-start">
+              <div className="w-[70px] h-[70px] lg:w-[150px] lg:h-[150px]  text-sm lg:text-lg   bg-orange-400  rounded-full   flex items-center justify-center">
                 <span>Resume</span>
               </div>
-              <div className="w-[180px] h-[180px]  bg-red-500  rounded-full ml-3 flex items-center justify-center">
+              <div className="w-[70px] h-[70px] lg:w-[150px] lg:h-[150px]  text-sm lg:text-lg  bg-red-500  rounded-full ml-3 flex items-center justify-center">
                 <span>Projects</span>
               </div>
-              <div className="w-[180px] h-[180px]  bg-blue-300 rounded-full  ml-3 flex items-center justify-center">
+              <div className="w-[70px] h-[70px] lg:w-[150px]  lg:h-[150px] text-sm lg:text-lg bg-blue-300 rounded-full  ml-3 flex items-center justify-center">
                 <span>Contacts</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <footer>hi</footer>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Hamburger from "hamburger-react";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="flex justify-between shadow-md h-[70px] p-4 items-center fixed w-full top-0 bg-white z-30">
+      <div className="flex items-center justify-center ml-4 lg:ml-10">
+        <span className="w-4 h-4 rounded-full bg-orange-500 lg:w-7 lg:h-7"></span>
+        <h2 className="font-bold text-lg lg:text-3xl ml-2 lg:ml-3">
+          <span className="text-orange-400 tracking-wider">H</span>assan
+        </h2>
+        <span className="text-sm ml-1 lg:text-lg mt-1">
+    Web dev
+        </span>
+      </div>
+      <div className="lg:hidden me-4">
+        <Hamburger toggled={menuOpen} toggle={toggleMenu} />
+      </div>
+      <div
+        className={` lg:flex items-center  ${
+          menuOpen
+            ? "absolute flex flex-col right-0 w-full h-[100vh] bg-white z-50 shadow-xl rounded-lg top-12 "
+            : "hidden "
+        }   `}
+      >
+        <a href="#" className="lg:mx-2 rounded-lg hover:bg-blue-200 px-3">
+          Resume
+        </a>
+        |
+        <a href="#" className="lg:mx-2  rounded-lg hover:bg-blue-200 px-3">
+          Projects
+        </a>
+        |
+        <a href="#" className="lg:mx-2  rounded-lg hover:bg-blue-200 px-3">
+          Contact
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

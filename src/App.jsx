300c596e197c83f9ff119839436c05873d6956f9
Class04 <hassankhan5uy99@gmail.com>
2024-01-20 19:23:35 +0500

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,66 +8,62 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div id="loader">
-        <div id="wrapperload">
-          <div className="elem">
-            <h2 className="lblue">@Hassan</h2>
-          </div>
-          <div className="elem">
-            <h3>UI/UX Designer</h3>
-          </div>
-          <div className="elem">
-            <h3>loves to help brands.</h3>
-          </div>
-          <div className="elem">
-            <h2 className="lblue">@Hassan</h2>
-          </div>
+    <div className="h-[100%] w-[100%]">
+      <nav className="flex justify-between shadow-md h-[70px] p-4 items-center ">
+        <div className="flex items-center justify-center ml-10">
+          <span className="w-[25px] h-[25px] rounded-2xl  bg-orange-500"></span>
+          <h2 className="font-bold text-2xl ml-3">Hassan</h2>
+          <span className="text-md ml-2 mt-1">Web dev</span>
         </div>
-      </div>
-      <nav>
-        <h1 className="lblue">@Hassan</h1>
-        <div id="links">
-          <a className="navitem" href="#">
-            Home
+        <div className="me-12">
+          <a href="" className="mx-2">
+            Resume
           </a>
-          <a className="navitem" href="#">
+          |
+          <a href="" className="mx-2">
+            Projects
+          </a>
+          |
+          <a href="" className="mx-2">
             Contact
           </a>
-          <i className="navitem ri-search-line"></i>
-          <i id="menu" className="navitem ri-menu-2-line"></i>
         </div>
       </nav>
-      <div id="sections">
-        <img className="leftitem" id="splash" src={rightSplash} alt="" />
-        <div id="left">
-          <div id="smline"></div>
-          <h4 className="leftitem">Hello, I am 👋</h4>
-          {/* <img className="leftitem" src={namepng} alt=""/> */}
-          <h4 className="lblue">Hassan</h4>
-          <h5 className="leftitem">
-            a designer from <span id="lblue">Pakistan.</span>
-          </h5>
-          <div className="leftitem" id="playbtn">
-            <img src={playbtn} alt="" />
-            <h5>
-              see my <span className="bold">work profile</span>
-            </h5>
-          </div>
-          <div className="leftitem" id="contact">
-            <h3>Contact me</h3>
-            <h6>
-              Email : <span>someone@email.com</span>
-            </h6>
-            <h6>
-              Contact Number: <span>+91-123456789</span>
-            </h6>
-          </div>
+      {/* Home */}
+      <div className="h-[100%] w-[100%]  flex mt-32 items-center">
+        <div className="h-[100%] w-[50%]  flex items-center justify-end p-8">
+          <img src={mainimg} className="w-[60%]  rounded-full grayscale" />
         </div>
-        <div id="right">
-          <img src={mainimg} alt="" />
+
+        <div className="h-full w-[50%] px-8 ">
+          <div className="h-full w-full">
+            <div className="mb-12">
+              <h2 className="font-bold text-8xl">Hello</h2>
+            </div>
+            <div className="mt-2">
+              <h3 className="text-2xl font-bold">A Bit About Me</h3>
+              <p className="text-lg ">
+                Lorem ipsum dolor sit amet, consectetur <br />
+                Lorem ipsum dolor sit amet, consectetur dolor
+                <br />
+                Lorem ipsum dolor amet, consectetur
+              </p>
+            </div>
+            <div className="flex w-full h-full mt-10">
+              <div className="w-[9.4vw] bg-orange-400 h-[20vh] rounded-full  ml-3 flex items-center justify-center">
+                <span>Resume</span>
+              </div>
+              <div className="w-[9.4vw] bg-red-500 h-[20vh] rounded-full ml-3 flex items-center justify-center">
+                <span>Projects</span>
+              </div>
+              <div className="w-[9.4vw] bg-blue-300 h-[20vh] rounded-full  ml-3 flex items-center justify-center">
+                <span>Contacts</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <footer>hi</footer>
     </div>
   );
 }

80aa8b56869e2d5dfabe5cc581276ec63e53df50
Class04 <hassankhan5uy99@gmail.com>
2024-01-23 03:33:28 +0500

    13th

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,26 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import projects from "../assets/projects.jpg";
-import gsap from "gsap";
-function Projects() {
-  let imgref1 = useRef(null);
-  let imgref2 = useRef(null);
-  let imgref3 = useRef(null);
-  let proName1 = useRef(null);
-  let proName2 = useRef(null);
-  let proName3 = useRef(null);
-  useEffect(() => {
-    gsap.to([imgref1, imgref2 ,imgref3], {
-      opacity:1,
-      duration: 1,
-      x: -20,
-    });
-    gsap.to([proName1, proName2 ,proName3], {
-      opacity:1,
-      duration: 1,
-      x: 5,
-    });
-  }, []);
 
+function Projects() {
   return (
     <div className="h-auto w-full  flex items-center justify-center p-6  mt-24 lg:mt-52">
       <div className="h-full w-full lg:w-[75%] ">
@@ -28,49 +9,35 @@ function Projects() {
         <div className="">
           <div className="flex mt-10 lg:mt-40 flex-col lg:flex-row  rounded-md">
             <div className="">
-              <h2 ref={(el) => (proName1=el)} className="text-2xl opacity-0  font-bold">Project Name 01</h2>
+              <h2 className="text-2xl font-bold">Project Name 01</h2>
               <p className=" lg:w-[75%] mb-5 text-justify tracking-wide mt-8 text-xl">
-                this is my first portfolio and welcome to my portfolio this is
-                my first portfolio and welcome to my portfolio this is my first
-                portfolio and welcome to my portfolio this is my first portfolio
-                and welcome to my portfolio.
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio. 
               </p>
             </div>
-            <img
-              src={projects}
-              ref={(el) => {
-                imgref1 = el;
-              }}
-              className="w-[500px] opacity-0 ml-auto rounded-md bg-blue-300"
-              alt=""
-            />
+            <img src={projects} className="w-[500px] ml-auto rounded-md bg-blue-300" alt="" />
           </div>
         </div>
         <div className="">
           <div className="flex mt-10 lg:mt-40 flex-col lg:flex-row ">
             <div className="">
-              <h2 ref={(el) => (proName2=el)} className="text-2xl opacity-0  font-bold">Project Name 02</h2>
+              <h2 className="text-2xl font-bold">Project Name 02</h2>
               <p className=" lg:w-[75%] mb-5 text-justify tracking-wide mt-8 text-xl">
-                this is my first portfolio and welcome to my portfolio this is
-                my first portfolio and welcome to my portfolio this is my first
-                portfolio and welcome to my portfolio this is my first portfolio
-                and welcome to my portfolio.
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio 
+                this is my first portfolio and welcome to my portfolio. 
               </p>
             </div>
-            <img
-             ref={(el) => {
-              imgref2 = el;
-            }}
-              src={projects}
-              className="w-[500px] ml-auto opacity-0 rounded-md  bg-blue-300"
-              alt=""
-            />
+            <img src={projects} className="w-[500px] ml-auto rounded-md  bg-blue-300" alt="" />
           </div>
         </div>
         <div>
           <div className="flex mt-10 lg:mt-40  mb-5 lg:mb-12  flex-col lg:flex-row">
             <div>
-              <h2 ref={(el) => (proName3=el)} className=" text-2xl opacity-0  font-bold">Project Name 03</h2>
+              <h2 className="text-2xl font-bold">Project Name 03</h2>
               <p className="lg:w-[75%] text-justify h-full mb-5 tracking-wide mt-8 text-xl">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Eligendi cumque enim eaque iure adipisci reiciendis porro illum
@@ -78,14 +45,7 @@ function Projects() {
                 facere iusto distinctio recusandae unde.
               </p>
             </div>
-            <img
-              src={projects}
-              ref={(el) => {
-                imgref3 = el;
-              }}
-              className=" lg:mt-0 w-[500px] opacity-0 ml-auto rounded-md "
-              alt=""
-            />
+            <img src={projects} className=" lg:mt-0 w-[500px] ml-auto rounded-md " alt="" />
           </div>
         </div>
       </div>

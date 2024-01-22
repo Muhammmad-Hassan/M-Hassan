0e3d6aba0d5e29566e670e6c185a30743b17a46a
Class04 <hassankhan5uy99@gmail.com>
2024-01-23 03:29:46 +0500

    13th

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -5,11 +5,19 @@ function Projects() {
   let imgref1 = useRef(null);
   let imgref2 = useRef(null);
   let imgref3 = useRef(null);
+  let proName1 = useRef(null);
+  let proName2 = useRef(null);
+  let proName3 = useRef(null);
   useEffect(() => {
     gsap.to([imgref1, imgref2 ,imgref3], {
       opacity:1,
       duration: 1,
       x: -20,
+    });
+    gsap.to([proName1, proName2 ,proName3], {
+      opacity:1,
+      duration: 1,
+      x: 5,
     });
   }, []);
 
@@ -20,7 +28,7 @@ function Projects() {
         <div className="">
           <div className="flex mt-10 lg:mt-40 flex-col lg:flex-row  rounded-md">
             <div className="">
-              <h2 className="text-2xl font-bold">Project Name 01</h2>
+              <h2 ref={(el) => (proName1=el)} className="text-2xl opacity-0  font-bold">Project Name 01</h2>
               <p className=" lg:w-[75%] mb-5 text-justify tracking-wide mt-8 text-xl">
                 this is my first portfolio and welcome to my portfolio this is
                 my first portfolio and welcome to my portfolio this is my first
@@ -41,7 +49,7 @@ function Projects() {
         <div className="">
           <div className="flex mt-10 lg:mt-40 flex-col lg:flex-row ">
             <div className="">
-              <h2 className="text-2xl font-bold">Project Name 02</h2>
+              <h2 ref={(el) => (proName2=el)} className="text-2xl opacity-0  font-bold">Project Name 02</h2>
               <p className=" lg:w-[75%] mb-5 text-justify tracking-wide mt-8 text-xl">
                 this is my first portfolio and welcome to my portfolio this is
                 my first portfolio and welcome to my portfolio this is my first
@@ -62,7 +70,7 @@ function Projects() {
         <div>
           <div className="flex mt-10 lg:mt-40  mb-5 lg:mb-12  flex-col lg:flex-row">
             <div>
-              <h2 className="text-2xl font-bold">Project Name 03</h2>
+              <h2 ref={(el) => (proName3=el)} className=" text-2xl opacity-0  font-bold">Project Name 03</h2>
               <p className="lg:w-[75%] text-justify h-full mb-5 tracking-wide mt-8 text-xl">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Eligendi cumque enim eaque iure adipisci reiciendis porro illum

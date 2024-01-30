79f263df0321f0b97ea956a0d7226f4644941643
Class04 <hassankhan5uy99@gmail.com>
2024-01-30 18:29:07 +0500

    another commit 2

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
+import React from "react";
 import contact from '../assets/contact2.avif'
 
 function Contact() {
-    let fullPage = useRef(null);
-
-    useEffect(() => {
-      gsap.to(fullPage, {
-        opacity: 1,
-        duration: 2.2,
-        ease: "power3.out",
-      });
-    },[])
 
   return (
-    <div  ref={(el) => (fullPage = el)} className="w-full h-full opacity-0">
+    <div   className="w-full h-full ">
         <img src={contact} alt="" className="w-full h-full object-cover opacity-80" />
     </div>
   )
